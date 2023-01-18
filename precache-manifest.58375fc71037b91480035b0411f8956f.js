200dfef04ca1e98cf2a6e333ff17c8357d931e98
Shailendra Kumar Saini <shailsinghadiya456@gmail.com>
2023-01-18 18:01:58 +0530

    Updates

---
+++ precache-manifest.58375fc71037b91480035b0411f8956f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4561c253f2430e8d8e53",
+    "url": "/my-app/static/css/main.49e8cf7c.chunk.css"
+  },
+  {
+    "revision": "4561c253f2430e8d8e53",
+    "url": "/my-app/static/js/main.4561c253.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/my-app/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "d97cf6c7d54af05937c4",
+    "url": "/my-app/static/js/2.d97cf6c7.chunk.js"
+  },
+  {
+    "revision": "fe08e4d762982826d1b1",
+    "url": "/my-app/static/js/runtime~main.fe08e4d7.js"
+  },
+  {
+    "revision": "102ed0220855e146edb7e74b8e7e54df",
+    "url": "/my-app/index.html"
+  }
+];
